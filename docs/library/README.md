f2e34046c374166b919b72a7feb5ed0ad5f322e6
SquabbyZ <601709253@qq.com>
2019-09-27 15:00:54 +0800

    123

--- docs/library/README.md
+++ docs/library/README.md
@@ -1,43 +1,44 @@
 ## 常用的网站大排档
 
-|          串名                                  |                                                      柜号                                                      |
-| :--------------------: | :------------------------------------------------------------------------------------------------------------: |
-|      MDN Web 文档                           |                  [https://developer.mozilla.org/zh-CN/](https://developer.mozilla.org/zh-CN/)                  |
-|      echarts 官网                           |               [https://www.echartsjs.com/zh/index.html](https://www.echartsjs.com/zh/index.html)               |
-|      Lodash 官网                        |                             [https://www.lodashjs.com/](https://www.lodashjs.com/)                             |
-|   Underscore.js 官网                       |                   [https://www.html.cn/doc/underscore/](https://www.html.cn/doc/underscore/)                   |
-|    learnCloud 官网                    |                                 [https://leancloud.cn/](https://leancloud.cn/)                                 |
-|     cesium 中文网                          |          [http://cesium.xin/wordpress/archives/16.html](http://cesium.xin/wordpress/archives/16.html)          |
-|      mkcert 证书                    |                 [https://github.com/FiloSottile/mkcert](https://github.com/FiloSottile/mkcert)                 |
-|        Mongodb                 |                              [https://www.mongodb.com/](https://www.mongodb.com/)                              |
-| nginx 各个版本下载地址  |                                     [http://nginx.org/](http://nginx.org/)                                     |
-|  elasticSearch 数据库                          |                            [https://www.elastic.co/cn/](https://www.elastic.co/cn/)                            |
-|      Cnode 中文网                        |                                  [https://cnodejs.org/](https://cnodejs.org/)                                  |
-|      express 官网                       |                                 [http://expressjs.com/](http://expressjs.com/)                                 |
-|         Axios                                   |                               [http://www.axios-js.com](http://www.axios-js.com)                               |
-|      svg.js 官网                                 |                                    [https://svgjs.com/](https://svgjs.com/)                                    |
-|     three.js 官网                            |                                  [https://threejs.org/](https://threejs.org/)                                  |
-|       D3.js 官网                                 |                                     [https://d3js.org/](https://d3js.org/)                                     |
-|      ogma 关系图                            |          [https://doc.linkurio.us/ogma/latest/api.html](https://doc.linkurio.us/ogma/latest/api.html)          |
-|    在线图片工具网站                        |                   [https://app.xunjiepdf.com/img2icon/](https://app.xunjiepdf.com/img2icon/)                   |
-|   在线制作 ico 图标                        |                                [http://www.bitbug.net/](http://www.bitbug.net/)                                |
-|   阿里巴巴矢量图标库                     |                              [https://www.iconfont.cn/](https://www.iconfont.cn/)                              |
-|      Font Awesome                          | [https://www.thinkcmf.com/font/font_awesome/icons.html](https://www.thinkcmf.com/font/font_awesome/icons.html) |
-|    fontawesome 图标(最近上不去)                         |                               [https://fontawesome.com](https://fontawesome.com)                               |
-|  在线字体格式转换网站                           |                 [https://www.fontke.com/tool/convfont/](https://www.fontke.com/tool/convfont/)                 |
-|         求字体                          |                               [http://www.qiuziti.com/](http://www.qiuziti.com/)                               |
-|         Swiper                              |                            [https://www.swiper.com.cn/](https://www.swiper.com.cn/)                            |
-|        jsdelivr                             |                             [https://www.jsdelivr.com/](https://www.jsdelivr.com/)                             |
-|文件格式在线转换免费网站|[http://www.pdfdo.com/pdf-to-html.aspx](http://www.pdfdo.com/pdf-to-html.aspx)|
+|             串名             |                                                      柜号                                                      |
+| :--------------------------: | :------------------------------------------------------------------------------------------------------------: |
+|         MDN Web 文档         |                  [https://developer.mozilla.org/zh-CN/](https://developer.mozilla.org/zh-CN/)                  |
+|         echarts 官网         |               [https://www.echartsjs.com/zh/index.html](https://www.echartsjs.com/zh/index.html)               |
+|         Lodash 官网          |                             [https://www.lodashjs.com/](https://www.lodashjs.com/)                             |
+|      Underscore.js 官网      |                   [https://www.html.cn/doc/underscore/](https://www.html.cn/doc/underscore/)                   |
+|       learnCloud 官网        |                                 [https://leancloud.cn/](https://leancloud.cn/)                                 |
+|        cesium 中文网         |          [http://cesium.xin/wordpress/archives/16.html](http://cesium.xin/wordpress/archives/16.html)          |
+|         mkcert 证书          |                 [https://github.com/FiloSottile/mkcert](https://github.com/FiloSottile/mkcert)                 |
+|           Mongodb            |                              [https://www.mongodb.com/](https://www.mongodb.com/)                              |
+|    nginx 各个版本下载地址    |                                     [http://nginx.org/](http://nginx.org/)                                     |
+|     elasticSearch 数据库     |                            [https://www.elastic.co/cn/](https://www.elastic.co/cn/)                            |
+|         Cnode 中文网         |                                  [https://cnodejs.org/](https://cnodejs.org/)                                  |
+|         express 官网         |                                 [http://expressjs.com/](http://expressjs.com/)                                 |
+|            Axios             |                               [http://www.axios-js.com](http://www.axios-js.com)                               |
+|         svg.js 官网          |                                    [https://svgjs.com/](https://svgjs.com/)                                    |
+|        three.js 官网         |                                  [https://threejs.org/](https://threejs.org/)                                  |
+|          D3.js 官网          |                                     [https://d3js.org/](https://d3js.org/)                                     |
+|         ogma 关系图          |          [https://doc.linkurio.us/ogma/latest/api.html](https://doc.linkurio.us/ogma/latest/api.html)          |
+|       在线图片工具网站       |                   [https://app.xunjiepdf.com/img2icon/](https://app.xunjiepdf.com/img2icon/)                   |
+|      在线制作 ico 图标       |                                [http://www.bitbug.net/](http://www.bitbug.net/)                                |
+|      阿里巴巴矢量图标库      |                              [https://www.iconfont.cn/](https://www.iconfont.cn/)                              |
+|         Font Awesome         | [https://www.thinkcmf.com/font/font_awesome/icons.html](https://www.thinkcmf.com/font/font_awesome/icons.html) |
+| fontawesome 图标(最近上不去) |                               [https://fontawesome.com](https://fontawesome.com)                               |
+|     在线字体格式转换网站     |                 [https://www.fontke.com/tool/convfont/](https://www.fontke.com/tool/convfont/)                 |
+|            求字体            |                               [http://www.qiuziti.com/](http://www.qiuziti.com/)                               |
+|            Swiper            |                            [https://www.swiper.com.cn/](https://www.swiper.com.cn/)                            |
+|           jsdelivr           |                             [https://www.jsdelivr.com/](https://www.jsdelivr.com/)                             |
+|   文件格式在线转换免费网站   |                 [http://www.pdfdo.com/pdf-to-html.aspx](http://www.pdfdo.com/pdf-to-html.aspx)                 |
 
 ## UI 回旋踢 ===> 一库
 
-|  回旋式分式   |                                   击打位置                                   |
-| :-----------: | :--------------------------------------------------------------------------: |
-|  MATERIAL-UI  |             [https://material-ui.com/](https://material-ui.com/)             |
-|  ELEMENT-UI   | [https://element.eleme.cn/2.0/#/zh-CN](https://element.eleme.cn/2.0/#/zh-CN) |
-|     iview     |             [https://www.iviewui.com/](https://www.iviewui.com/)             |
-| Ant Design-UI |          [https://ant.design/index-cn](https://ant.design/index-cn)          |
+|  回旋式分式   |                                     击打位置                                     |
+| :-----------: | :------------------------------------------------------------------------------: |
+|  MATERIAL-UI  |               [https://material-ui.com/](https://material-ui.com/)               |
+|  ELEMENT-UI   |   [https://element.eleme.cn/2.0/#/zh-CN](https://element.eleme.cn/2.0/#/zh-CN)   |
+|     iview     |               [https://www.iviewui.com/](https://www.iviewui.com/)               |
+| Ant Design-UI |            [https://ant.design/index-cn](https://ant.design/index-cn)            |
+|    Cube-ui    | [https://didi.github.io/cube-ui/#/zh-CN](https://didi.github.io/cube-ui/#/zh-CN) |
 
 ## Vue 组合拳
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "223479d0c1f90d627505451d09c305f1"
+    "revision": "ba1eb30a1fc09f23b37811b627f20e51"
   },
   {
     "url": "assets/css/0.styles.8d0ef34b.css",
@@ -50,16 +50,16 @@ self.__precacheManifest = [
     "revision": "8f016d187e085f5cd8be9a69c114fa14"
   },
   {
-    "url": "assets/js/11.432dfa9f.js",
-    "revision": "a11ab47da8f9856bada27cdf9247b232"
+    "url": "assets/js/11.bbab5902.js",
+    "revision": "1d2053c6f839e166db958745daf71697"
   },
   {
-    "url": "assets/js/12.f1cdbe0f.js",
-    "revision": "9fb604de3b5cbeff1caaf4b6d51fb7dc"
+    "url": "assets/js/12.bcee234b.js",
+    "revision": "c1923a351852d322e75c1714bbdd5fca"
   },
   {
-    "url": "assets/js/13.469a725a.js",
-    "revision": "f20c46eabb87d9574506f4a286591b9e"
+    "url": "assets/js/13.eb8e4fb7.js",
+    "revision": "c9a76da3a77397398af04061ddee1623"
   },
   {
     "url": "assets/js/14.0e76990a.js",
@@ -74,52 +74,52 @@ self.__precacheManifest = [
     "revision": "d91f2143cc36882a3b347540010c8107"
   },
   {
-    "url": "assets/js/17.03169d38.js",
-    "revision": "834b69d18eae9a6e47b5039989a74283"
+    "url": "assets/js/17.f999c2c2.js",
+    "revision": "783b0c3c795ebebdd0d70fda18e93ebb"
   },
   {
     "url": "assets/js/18.5d73a34b.js",
     "revision": "3341d034cb8c0ef04a177588b7c158ed"
   },
   {
-    "url": "assets/js/19.c384a4d7.js",
-    "revision": "7cc5afacae16230c93c4bbfcb4fcfc23"
+    "url": "assets/js/19.f279e581.js",
+    "revision": "5abb2e2d61deeb1632836974fa92b1a3"
   },
   {
-    "url": "assets/js/20.bc6dba35.js",
-    "revision": "455ad08bc407a00dc9c6d9d7d817e77f"
+    "url": "assets/js/20.bd8223c0.js",
+    "revision": "16d75f6118df7177b378582f6f22a877"
   },
   {
-    "url": "assets/js/21.192e2888.js",
-    "revision": "563d19a3f35193eb31fead56f9b05a42"
+    "url": "assets/js/21.e5feff96.js",
+    "revision": "ca8b9756e275f28d9a0144c409cad547"
   },
   {
-    "url": "assets/js/22.388002b1.js",
-    "revision": "669786c975d9f1dfd60211d9194c852f"
+    "url": "assets/js/22.19b280bf.js",
+    "revision": "d9603f8ba9089650a16bf1a59d2fda90"
   },
   {
     "url": "assets/js/23.110077c1.js",
     "revision": "795313effb80eb7bf0d629df4126b1d9"
   },
   {
-    "url": "assets/js/24.731911f7.js",
-    "revision": "595ab24276e77541330333ad83bb4299"
+    "url": "assets/js/24.a593e533.js",
+    "revision": "cf7897032df55dfd53477ca3397b0b51"
   },
   {
-    "url": "assets/js/25.bb6f9802.js",
-    "revision": "88659eea57cbe0211ac6a39dd3e6126e"
+    "url": "assets/js/25.a6fc4af7.js",
+    "revision": "d5d32147041ecdf0616fe5c654f93fbe"
   },
   {
-    "url": "assets/js/26.e656e416.js",
-    "revision": "83a45538881c0e5aeaca89c9183a28a3"
+    "url": "assets/js/26.32ed3169.js",
+    "revision": "431d37ba48c0c16ff1653389d9c4ec69"
   },
   {
     "url": "assets/js/27.660b1a66.js",
     "revision": "b6ef7c360fb15fc387776fbc7c14ab70"
   },
   {
-    "url": "assets/js/28.3159af37.js",
-    "revision": "5af34347e95fcc6c2b5f2c098b01834b"
+    "url": "assets/js/28.08be4477.js",
+    "revision": "f2d0ec00c246ad81af9773af9d714ff5"
   },
   {
     "url": "assets/js/29.464c545f.js",
@@ -162,8 +162,8 @@ self.__precacheManifest = [
     "revision": "68bffbd481c90f3ffb634296d8226ac7"
   },
   {
-    "url": "assets/js/app.3a95dbfa.js",
-    "revision": "9a9042b34d01a2b0bd0bb160d89e27fc"
+    "url": "assets/js/app.c955756f.js",
+    "revision": "2eb86f21c185773db9509cec993fb6de"
   },
   {
     "url": "assets/js/vendors~flowchart.cb9b614f.js",
@@ -175,59 +175,59 @@ self.__precacheManifest = [
   },
   {
     "url": "category/Centos.html",
-    "revision": "d1331a98c5326a1e7fea19d5b3aa7c52"
+    "revision": "5121964e52f338d90e9ac83b3952d525"
   },
   {
     "url": "category/Echarts.html",
-    "revision": "968333c37704cb660f080afc45489e41"
+    "revision": "21fa5600bbd57203a89aa1f2e84dcd7e"
   },
   {
     "url": "category/Html5.html",
-    "revision": "29308c1f0b9c338281d8810a327e95fc"
+    "revision": "fe4f21ffc204030e4afa79a265d53f74"
   },
   {
     "url": "category/index.html",
-    "revision": "98f93b4d2c46eedaa9fbedddfe3ea1e5"
+    "revision": "dc665e591a90c7561e4fea83eb70e978"
   },
   {
     "url": "category/JavaScript.html",
-    "revision": "52e910395aa24a15803be86d17624713"
+    "revision": "90b8e2dba394c323b44627355adba5cb"
   },
   {
     "url": "category/MySql.html",
-    "revision": "51a52d289756518759acbe646d9ca5be"
+    "revision": "7f17d8da7fb7290072de130c0cccbdb0"
   },
   {
     "url": "category/Nginx.html",
-    "revision": "b36041e3df3506bb0011307de765e189"
+    "revision": "96e6996846f6ca7c63cdecb5c471bdb8"
   },
   {
     "url": "category/Node.js.html",
-    "revision": "b32f99b311ba59aee94caf63d3c01d41"
+    "revision": "2d8d3a6606bf27a51f0b41ec06194ee5"
   },
   {
     "url": "category/React.html",
-    "revision": "e78a32d007e31a42864e6bf306ca05d4"
+    "revision": "354e502f5f060c9cff5e28ad247568d7"
   },
   {
     "url": "category/Vue.html",
-    "revision": "eb886c9b4e20df1b098c3a23e413188a"
+    "revision": "1cb4f2843832ef2451b72a29bac574b2"
   },
   {
     "url": "category/Wechatapplet.html",
-    "revision": "9bbb157cb2f9a8c5f60571043089fe2a"
+    "revision": "0b46cf947289923435f6ecdd01fc43f6"
   },
   {
     "url": "category/win7.html",
-    "revision": "1dc93e25bc097eed7c2fc140c0ef70fe"
+    "revision": "135ef4af930174a22860683b038f3164"
   },
   {
     "url": "index.html",
-    "revision": "e5c0f4d2733d3bb87e050b497ff0ef93"
+    "revision": "18652d93c48376b03a7aad43d84e8bf0"
   },
   {
     "url": "library/index.html",
-    "revision": "07a7e8ef9e78aae294f19ffb292db80a"
+    "revision": "dce24003a51c3e28209c0ab149c7fc82"
   },
   {
     "url": "live2d/hibiki/assets/moc/hibiki.2048/texture_00.png",
@@ -247,127 +247,127 @@ self.__precacheManifest = [
   },
   {
     "url": "tag/Centos.html",
-    "revision": "4d17efc5d033658144adc1009b02e4ff"
+    "revision": "189715ab9e8368ed6e15e3cc8f3e2941"
   },
   {
     "url": "tag/Echarts.html",
-    "revision": "6bc210338caaa707bcaf9746f51a8b4b"
+    "revision": "8e999fe31579457364532fee2b7baa23"
   },
   {
     "url": "tag/Html5.html",
-    "revision": "5287981c0e58c31fa0d4267ce0cda961"
+    "revision": "12300d1e7fd17ef67297538903dad518"
   },
   {
     "url": "tag/index.html",
-    "revision": "b8e47e458e84ca131fc8014971155394"
+    "revision": "beb8b385baf8da81caab10e3665861d3"
   },
   {
     "url": "tag/JavaScript.html",
-    "revision": "b74db179f6df9403d6422d986d61e3f0"
+    "revision": "a29b3b1a2dcdadf6225bac02fc79b500"
   },
   {
     "url": "tag/MySql.html",
-    "revision": "16e0030586278e8d0de7320d1089711d"
+    "revision": "9f139d5e8f0bec0d1a5c5f24e5c66f4b"
   },
   {
     "url": "tag/Nginx.html",
-    "revision": "d982071b282c5896f9563e8b6d81c603"
+    "revision": "d4edca03980f2c6632bcb691c6fcd699"
   },
   {
     "url": "tag/Node.js.html",
-    "revision": "9a60903767b4d936dcd7457108114a8f"
+    "revision": "a02b7f2b53a189cfcad1be47d74763ef"
   },
   {
     "url": "tag/React.html",
-    "revision": "9f5a2847c13957914b107a979b192778"
+    "revision": "6767524c3c5dd2549a360036ec71ba6e"
   },
   {
     "url": "tag/Vue.html",
-    "revision": "633e2ba0131617e6ed0d4c4b5e28c3a6"
+    "revision": "30d2d6ef382a5cc4c0fd4c84a51e862c"
   },
   {
     "url": "tag/Wechatapplet.html",
-    "revision": "f0e03cf3dccd12520f0d282a456fdf02"
+    "revision": "7b5c5357339f1b4b5477889609c47603"
   },
   {
     "url": "tag/win7.html",
-    "revision": "108e52e00b6ba922ac5b16278a8080c1"
+    "revision": "53ad73460a3766d8345d91d4bfc8c627"
   },
   {
     "url": "timeLine/index.html",
-    "revision": "6dafff538dc8d4994e9b6a93d47c8636"
+    "revision": "2eb3434131b98d2e8a313e2e3fb4ae76"
   },
   {
     "url": "views/Centos/1.html",
-    "revision": "fbffb811bba5fada6234e3bfb907544a"
+    "revision": "2c8496ceb0a86e27dc805042392439a2"
   },
   {
     "url": "views/Centos/2.html",
-    "revision": "b139400754ccdb540e592618faf72b97"
+    "revision": "43b826353c8bd26bb03781d7bcf8ddff"
   },
   {
     "url": "views/echarts/1.html",
-    "revision": "49c6f1ed9dd39d0f47f82dbfdb6d79dc"
+    "revision": "dd1a0855dfe2b19c4be4f2db092c492e"
   },
   {
     "url": "views/elasticSearch/1.html",
-    "revision": "b85fdb53b29a5680ebf2451f472a8b8f"
+    "revision": "558499d3530721524dcb60f063395057"
   },
   {
     "url": "views/Html5/1.html",
-    "revision": "2cc30c3c7d559ca176778ca1bdeea68b"
+    "revision": "13cdd8220e98b049fb51144c2adfd9dd"
   },
   {
     "url": "views/Html5/2.html",
-    "revision": "97d6e9e7f10ce09df3cfda068209085d"
+    "revision": "d621b2e6d7011fc328a25bcf94b77c05"
   },
   {
     "url": "views/JavaScript/1.html",
-    "revision": "db44c1458457c4befca86c15bb5cfc09"
+    "revision": "d10cb82f105e3c8e742a9e86ba6eb102"
   },
   {
     "url": "views/JavaScript/2.html",
-    "revision": "38b6612cd000fb469cb6284d8fb93c67"
+    "revision": "b6cebd64e59a037785f62cec3e815e69"
   },
   {
     "url": "views/JavaScript/3.html",
-    "revision": "f13a220d150360eb7adac0f7da11484d"
+    "revision": "8c0b756f5edc68c2f30664a55c2c45ea"
   },
   {
     "url": "views/MySql/1.html",
-    "revision": "159e642b66b50632a43f22c2152f5682"
+    "revision": "513185e11b17c1b88fa879f49eda3920"
   },
   {
     "url": "views/nginx/1.html",
-    "revision": "46859f251566c8cf3a8f742e8b6bf8c7"
+    "revision": "d0a2b697ec00bcfd280667c492e8fbfa"
   },
   {
     "url": "views/nodejs/1.html",
-    "revision": "7c50cf6fb303ae18f1d0118ad1af0ed0"
+    "revision": "c0b10eed5742e14c2c576d9787437f26"
   },
   {
     "url": "views/nodejs/2.html",
-    "revision": "1a577b7928ffc56ade95f25f03d142c1"
+    "revision": "12d4fdd7610d967d60a1246181ceb181"
   },
   {
     "url": "views/other/guide.html",
-    "revision": "89b82ad3bae281bef057a1ef45cf4206"
+    "revision": "3ef0c9b9435ffbf15a51302b43e1cdd5"
   },
   {
     "url": "views/react/1.html",
-    "revision": "8e9062624849cf81e93c8e86e117c83c"
+    "revision": "e00989bc29182b823fc6bd0ba64dfdec"
   },
   {
     "url": "views/vue/1.html",
-    "revision": "4fabf479416c3a64bec3a8c993f4318e"
+    "revision": "9bb17ac72afac7a1fdfaac4b18aa98ef"
   },
   {
     "url": "views/Wechatapplet/1.html",
-    "revision": "98a6e9c6b2aeb004858d0c114630efba"
+    "revision": "b005e2aa9068bc08b357597949ee5cd2"
   },
   {
     "url": "views/win7/1.html",
-    "revision": "4ca61cea231a98040205c8e6e0d411b4"
+    "revision": "35575e84ef7a3a95e088822505a508e0"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
